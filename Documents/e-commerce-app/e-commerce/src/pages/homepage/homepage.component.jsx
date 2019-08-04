88b0bdb0fca5953e3b6d616a9d2a0721d4e473ef
tetecruzc <stephaniecruzcastelli@hotmail.com>
2019-08-04 13:43:30 -0400

    menu-items del home listos

---
+++ Documents/e-commerce-app/e-commerce/src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '/Users/Tete/Documents/e-commerce-app/e-commerce/src/pages/homepage/homepage.styles.scss';
+import Directory from '/Users/Tete/Documents/e-commerce-app/e-commerce/src/components/directory/directory.component.jsx';
+const HomePage = () => (
+    <div className='homepage'>
+       <Directory/>
+    </div>
+);
+
+export default HomePage;
