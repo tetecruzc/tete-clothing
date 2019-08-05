ef68e4c2639476ac7108ed5d976daf916dbb75c6
tetecruzc <stephaniecruzcastelli@hotmail.com>
2019-08-05 18:58:56 -0400

    header component added

--- Documents/e-commerce-app/e-commerce/src/App.js
+++ Documents/e-commerce-app/e-commerce/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import {Switch,Route} from 'react-router-dom';
-
+import Header from './components/header/header.component';
 
 /*const Hats = props =>{
   console.log(props);
@@ -17,6 +17,7 @@ import {Switch,Route} from 'react-router-dom';
 function App() {
   return (
     <div>
+      <Header/>
      <Switch>
        <Route exact path='/' component={HomePage}/>
        <Route exact path='/shop' component={ShopPage}/>
